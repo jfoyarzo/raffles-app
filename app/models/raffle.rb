7a5ac20cf404c2461796f65cdefdb63ea42ddc31
jfoyarzo <foyarzo621@gmail.com>
2023-05-18 19:29:16 -0400

    Add raffle model validations

--- app/models/raffle.rb
+++ app/models/raffle.rb
@@ -5,4 +5,8 @@ class Raffle < ApplicationRecord
   has_many :tickets
   has_many :winners
   has_one_attached :image
+  validates :name, :description, :start_date, :end_date, :drawing_date, :max_tickets, presence: true
+  validates :max_tickets, numericality: { only_integer: true }
+  validates :end_date, comparison: { greater_than: :start_date }
+  validates :name, length: { in: 5..30 }
 end
