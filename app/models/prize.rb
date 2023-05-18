5edeaacb7e3dbde267aedae7bde716832601457a
jfoyarzo <foyarzo621@gmail.com>
2023-05-18 19:29:28 -0400

    Add prize model validations

--- app/models/prize.rb
+++ app/models/prize.rb
@@ -2,4 +2,7 @@ class Prize < ApplicationRecord
   belongs_to :raffle
   has_one :winner
   has_one_attached :image
+  validates :name, :place, presence: true
+  validates :place, numericality: { only_integer: true }
+  validates :name, length: { in: 5..30 }
 end
