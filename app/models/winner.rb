07904a454fbcbf47dada271685d4269a08dcd278
jfoyarzo <foyarzo621@gmail.com>
2023-05-18 19:29:38 -0400

    Add winner model validations

--- app/models/winner.rb
+++ app/models/winner.rb
@@ -2,4 +2,6 @@ class Winner < ApplicationRecord
   belongs_to :raffle
   belongs_to :participant
   belongs_to :prize
+  validates :place, :drawing_date, presence: true
+  validates :place, numericality: { only_integer: true }
 end
