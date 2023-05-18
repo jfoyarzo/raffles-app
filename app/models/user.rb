183d28af9752e8beda53c1c8d27eac6de0e873d7
jfoyarzo <foyarzo621@gmail.com>
2023-05-18 19:29:05 -0400

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :raffles
   has_many :participants
   has_one_attached :avatar
+  validates :full_name, presence: true
+  validates :full_name, length: { in: 5..50 }
 end
